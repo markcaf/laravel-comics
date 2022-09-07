8473c7b4b4a7098fb58c105b50f553b1d3f35648
markcaf <marcocafassog@gmail.com>
2022-09-07 18:53:54 +0200

    feat: add footer structure and markup

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,73 @@
+<footer>
+    <div class="footer-links">
+      <div class="container d-flex justify-content-between">
+        <div class="links mt-5 d-flex flex-wrap">
+          <div class="menu">
+            <h5>DC COMICS</h5>
+            <ul class="list-unstyled">
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+
+            <h5>SHOP</h5>
+            <ul class="list-unstyled">
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+            </ul>
+          </div>
+
+          <div class="menu">
+            <h5>DC</h5>
+            <ul class="list-unstyled">
+                <li>Terms Of Use</li>
+                <li>Privacy policy</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+          </div>
+
+          <div class="menu">
+            <h5>SITES</h5>
+            <ul class="list-unstyled">
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="logo-bg">
+          <img src="assets/images/dc-logo-bg.png" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-social py-4">
+      <div class="container d-flex justify-content-between">
+        <div class="cta-button text-white">SIGN-UP NOW!</div>
+
+        <div class="social d-flex align-items-center gap-3">
+          <div>FOLLOW US</div>
+          <img src="assets/images/footer-facebook.png" alt="facebook Logo" />
+          <img src="assets/images/footer-periscope.png" alt="periscope Logo" />
+          <img src="assets/images/footer-pinterest.png" alt="pinterest Logo" />
+          <img src="assets/images/footer-twitter.png" alt="twitter Logo" />
+          <img src="assets/images/footer-youtube.png" alt="youtube Logo" />
+        </div>
+      </div>
+    </div>
+</footer>
