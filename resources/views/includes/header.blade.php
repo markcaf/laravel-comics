9f9848d36759bff020bdd82fe0e5e250e38c123c
markcaf <marcocafassog@gmail.com>
2022-09-07 18:22:57 +0200

    feat: add header structure and markup

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,24 @@
+<header class="container">
+    <nav class="d-flex justify-content-between align-items-center py-3">
+        <div class="logo">
+            <img src="assets/images/dc-logo.png" alt="DC Universe Logo">
+        </div>
+
+        <ul class="list-unstyled">
+            <li><a>CHARACTERS</a></li>
+            <li>
+                <a class="active_link">
+                    COMICS
+                </a>
+            </li>
+            <li><a>MOVIES</a></li>
+            <li><a>TV</a></li>
+            <li><a>GAMES</a></li>
+            <li><a>COLLECTIBLES</a></li>
+            <li><a>VIDEO</a></li>
+            <li><a>FANS</a></li>
+            <li><a>NEWS</a></li>
+            <li><a>SHOP</a></li>
+        </ul>
+    </nav>
+</header>
